2f0144a5dfe3314387ff1322c8b6334887b3e983
Jumaiza <zaid.abujumaiza@gmail.com>
2022-10-16 19:13:24 -0400

    idk

--- HW_1/test.c
+++ HW_1/test.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main() {
-   printf("Hello World!");
+   printf("Hello World!\n");
+   char name[]="Zaid";
+   char city[] = {'B','u','d','d','L','a','k','e'};
+   printf("%p",&city+1);
    return 0;
 }
