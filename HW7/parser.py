0c42b934f2271f600b50a572c43fc4978b47d9ea
Jumaiza <zaid.abujumaiza@gmail.com>
2022-12-06 23:53:22 -0500

    parser done

--- HW7/parser.py
+++ HW7/parser.py
@@ -2,12 +2,26 @@ import sys
 import mysql.connector
 import xml.dom.minidom as domApi
 
+# def insert(cursor,vendor):
+#     query = 'INSERT INTO '+vendor'(SKU,ProductName,ProductDesc,ProductPrice,RemImgUrl,LocImgUrl,ReviewScore) VALUES (%s,%s,%s)'
+#     cursor.execute(query, ('0385514239','Origin',2995))
+
+# def update(cursor):
+#     query = 'UPDATE book SET price=%s WHERE isbn=%s'
+#     cursor.execute(query, (29.95,'0385514239'))
+try:
+    cnx = mysql.connector.connect(host='localhost', user='root', password='cs288', database='cs288')
+    cursor = cnx.cursor()
+    cursor.close()
+except mysql.connector.Error as err:
+    print(err)
+
 if sys.argv[1] == "vivid":
 
     doc = domApi.parse(sys.argv[2])
 
-    titleElement = doc.getElementsByTagName("title")
-    title = titleElement[0].childNodes[0].nodeValue
+    titleElements = doc.getElementsByTagName("title")
+    title = titleElements[0].childNodes[0].nodeValue
     sku = title.split("  | ")[1]
     name = title.split("  | ")[0]
     print("\nSKU:",sku)
@@ -53,6 +67,7 @@ if sys.argv[1] == "vivid":
     for aTag in aTags:
         if aTag.getAttribute('title')[:13] == "Product Image":
             link = aTag.getAttribute('href')
+            break
 
     print("Remote Image URL:",link)
 
@@ -63,57 +78,45 @@ if sys.argv[1] == "vivid":
 
 if sys.argv[1] == "lmp":
 
-    print("Hello")
-
     doc = domApi.parse(sys.argv[2])
 
     titleElement = doc.getElementsByTagName("title")
     title = titleElement[0].childNodes[0].nodeValue
-    sku = title.split("  | ")[1]
-    name = title.split("  | ")[0]
+    sku = title.split(" - ")[1]
+    name = title.split(" - ")[0]
     print("\nSKU:",sku)
     print("Name:",name)
 
     description = ""
     divs = doc.getElementsByTagName("div")
     for div in divs:
-        divId = div.getAttribute("id")
-        if divId == "productDescription":
+        divClass = div.getAttribute("class")
+        if divClass == "show_des border_box":
             pTags = div.getElementsByTagName("p")
             for pTag in pTags:
-                if len(pTag.childNodes)>1:
-                    for node in pTag.childNodes:
-                        if node.nodeType == node.TEXT_NODE:
-                            description += node.nodeValue
-                    break
-                else:
-                    for node in pTag.childNodes:
-                        if node.nodeType == node.TEXT_NODE:
-                            description += node.nodeValue
+                for node in pTag.childNodes:
+                    if node.nodeType == node.TEXT_NODE:
+                        description += node.nodeValue
             break
-
     print("Product Description:",description)
 
 
-    priceElement = doc.getElementsByTagName("p")[1]
+    spanTags = doc.getElementsByTagName("span")
     price = ""
-    if len(priceElement.childNodes)==1:
-        price = priceElement.childNodes[0].nodeValue
-        price = price.replace('$','')
-        price = price.replace(',','')
-    else:
-        priceElement = doc.getElementsByTagName("span")[9]
-        price = priceElement.childNodes[0].nodeValue
-        price = price.replace('Sale:$','')
-        price = price.replace(',','')
-    
+    for spanTag in spanTags:
+        spanClass = spanTag.getAttribute("class")
+        if spanClass == ".list_price":
+            price = spanTag.childNodes[0].nodeValue
+            price = price.replace('$','')
+            price = price.replace(',','')
+            break
     print("Price:",price)
 
     link = ""
-    aTags = doc.getElementsByTagName("a")
-    for aTag in aTags:
-        if aTag.getAttribute('title')[:13] == "Product Image":
-            link = aTag.getAttribute('href')
+    imgTags = doc.getElementsByTagName("img")
+    for imgTag in imgTags:
+        if imgTag.getAttribute('id') == "imgDisp":
+            link = imgTag.getAttribute('src')
 
     print("Remote Image URL:",link)
 
