e8b1c5964eb73f612aac6c13c7098234672c5c91
Jumaiza <zaid.abujumaiza@gmail.com>
2022-12-18 22:28:05 -0500

    hw8 done

--- Testing/FinalPractice/myhw8.c
+++ Testing/FinalPractice/myhw8.c
@@ -6,34 +6,34 @@
 
 int main(){
 
-    int i, id;
-    double x, y, r, pi;
+    double pi;
     int inCircleCount = 0;
     int pointCount = 250000;
-    int iterations = 0;
 
-    #pragma omp parallel num_threads(4)
+    #pragma omp parallel
     {
+        double x, y, r;
+        int id = omp_get_thread_num();
+        int nthreads = omp_get_num_threads();
+        int i;
 
-        for(i = 0; i<pointCount; i++){
+        for(i = id; i<pointCount; i+=nthreads){
             
             x = (double)rand() / RAND_MAX;
             y = (double)rand() / RAND_MAX;
             r = sqrt((x*x) + (y*y));
 
-            #pragma omp critical
-            {
-                if(r <= 1){
+            if(r <= 1){
+                #pragma omp critical
+                {
                     inCircleCount++;
                 }
-                iterations++;
             }
         }
     }
 
     pi = (double)inCircleCount/pointCount * 4;
     printf("PI Estimation: %f\n",pi);
-    printf("Iteration Count: %d", iterations);
     return 0;
 
 }
